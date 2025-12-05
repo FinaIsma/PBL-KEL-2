56ee7cf0c65259982ef816c25f9927c7c6ae13e5
Almafarel <almafarel.18@gmail.com>
2025-12-05 09:45:11 +0700

    Add bidang-fokus.php

---
+++ koneksiBidang.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$db   = "phpdatabase";
+$user = "postgres";
+$pass = "azon701";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
