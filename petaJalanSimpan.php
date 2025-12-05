276c9d82cb97da8412ccf6d78ac3234a5469bb9d
Adhe Widya <adhewidya378@gmail.com>
2025-12-05 20:49:44 +0700

    add petaJalanSimpan

--- petaJalanSimpan.php
+++ petaJalanSimpan.php
@@ -1,13 +1,11 @@
 <?php
-include("koneksi.php"); // file koneksi ke DB
+include("koneksi.php"); 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Ambil data dari form
     $peta_id = $_POST['peta_id'];
     $judul = $_POST['judul'];
     $tahun = $_POST['tahun'];
 
-    // Update data di database
     $query = "UPDATE peta_jalan 
               SET judul = $1, tahun = $2 
               WHERE peta_id = $3";
@@ -15,7 +13,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $result = pg_query_params($koneksi, $query, [$judul, $tahun, $peta_id]);
 
     if ($result) {
-        // Redirect ke halaman admin setelah berhasil
         header("Location: petaJalanAdmin.php");
         exit;
     } else {
